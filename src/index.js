d93e2fb8c7a83f545da8d2c7581cb1c79a8758fa
gladysleng <gladys_yinyuen@hotmail.com>
2020-03-18 17:30:22 -0400

    fix styling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Application from './components/application.js';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 
+    'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter> <Application /> </BrowserRouter>, document.getElementById('root')
